6c141ae6135d38e4475df2b7a9dedcc1c8e24158
luyaneluiz <luyane.luiz.ll@gmail.com>
2025-06-19 16:23:14 -0300

    Adiciona componentes Recommendations e RecipeCard para exibir sugestões de receitas

---
+++ src/components/cards/Recommendations.vue
@@ -0,0 +1,59 @@
+<script>
+    import RecipeCard from '../recipes/RecipeCard.vue'
+
+    export default {
+        name: 'Recommendations',
+        components: {
+            RecipeCard,
+        },
+        data() {
+            return {
+                recommendations: [
+                    {
+                        id: 1,
+                        image: 'https://img.spoonacular.com/recipes/716429-556x370.jpg',
+                        title: 'Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs',
+                    },
+                    {
+                        id: 2,
+                        image: 'https://img.spoonacular.com/recipes/716430-556x370.jpg',
+                        title: 'Spaghetti Carbonara',
+                    },
+                    {
+                        id: 3,
+                        image: 'https://img.spoonacular.com/recipes/716431-556x370.jpg',
+                        title: 'Vegetarian Chili',
+                    },
+                    {
+                        id: 4,
+                        image: 'https://img.spoonacular.com/recipes/716432-556x370.jpg',
+                        title: 'Chicken Tikka Masala',
+                    },
+                    {
+                        id: 5,
+                        image: 'https://img.spoonacular.com/recipes/716433-556x370.jpg',
+                        title: 'Beef Stroganoff',
+                    },
+                ],
+            }
+        },
+    }
+</script>
+
+<template>
+    <section class="card">
+        <div class="flex items-center justify-between">
+            <h1>Recommendations</h1>
+            <a href="" class="text-base">See More</a>
+        </div>
+
+        <div class="flex gap-4 overflow-x-scroll">
+            <RecipeCard
+                v-for="recipe in recommendations"
+                :key="recipe.id"
+                :image="recipe.image"
+                :title="recipe.title"
+            />
+        </div>
+    </section>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,14 @@
 <script>
     import WeeklyPlanner from '../components/cards/WeeklyPlanner.vue'
     import Statistics from '../components/cards/Statistics.vue'
+    import Recommendations from '../components/cards/Recommendations.vue'
 
     export default {
         name: 'Home',
         components: {
             Statistics,
             WeeklyPlanner,
+            Recommendations,
         },
     }
 </script>
@@ -15,5 +17,6 @@
     <div class="flex flex-col gap-5 px-5 py-3.5">
         <WeeklyPlanner />
         <Statistics />
+        <Recommendations />
     </div>
 </template>
