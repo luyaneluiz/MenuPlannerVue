b0cc3a3f0d8a65707c033ffa5bf7ff5c29172daf
luyane.luiz <luyane.luiz.ll@gmail.com>
2025-06-24 14:37:47 -0300

    Adiciona ícone de compartilhamento à seção da lista de compras na página inicial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
     import WeeklyPlanner from '../components/cards/WeeklyPlanner.vue'
     import Statistics from '../components/cards/Statistics.vue'
     import Recommendations from '../components/cards/Recommendations.vue'
+    import { Icon } from '@iconify/vue'
 
     export default {
         name: 'Home',
@@ -9,6 +10,7 @@
             Statistics,
             WeeklyPlanner,
             Recommendations,
+            Icon
         },
     }
 </script>
@@ -23,7 +25,13 @@
             <div class="flex flex-col gap-6 w-full overflow-hidden">
                 <div class="grid md:grid-cols-2 gap-6">
                     <Statistics />
-                    <Statistics />
+                    
+                    <section class="card">
+                        <div class="flex justify-between items-center">
+                            <h1>Your grocery list</h1>
+                            <Icon icon="codicon:share" width="16" />
+                        </div>
+                    </section>
                 </div>
 
                 <Recommendations />
